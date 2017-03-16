f2d9e395fdd7e52dd89dfe101f7f6fe0b681d0a2
Yang Li <zjzsliyang@gmail.com>
2017-03-16 20:11:39 +0800

    refactor(all): delete storyboard dependence

--- WWDC17/WWDC17/FirstViewController.swift
+++ WWDC17/WWDC17/FirstViewController.swift
@@ -11,30 +11,20 @@ import UIKit
 var times = 0
 
 class FirstViewController: UIViewController {
-  @IBOutlet weak var ballOutlet: UIButton!
   let ballButton = UIButton(frame: CGRect(x: 462, y: 633, width: 100, height: 100))
   var ballIsPressed = false
   var dynamicAnimator = UIDynamicAnimator()
-  
-  @IBAction func ballButton(_ sender: UIButton) {
-    ballGravityFall()
-  }
 
-  @IBAction func dragOutside(_ sender: UIButton) {
-    times = times + 1
-    print(times)
-    print(String(describing: sender))
-  }
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = UIColor(red: 242/255, green: 232/255, blue: 219/255, alpha: 1)
+    ballButton.setImage(UIImage(named: "ball"), for: .normal)
+    view.addSubview(ballButton)
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    ballOutlet.center = CGPoint(x: view.center.x, y: view.center.y)
-    
+    ballButton.center = CGPoint(x: view.center.x, y: view.center.y)
     let stack1View = UIImageView(image: UIImage(named: "stake1"))
     let stack2View = UIImageView(image: UIImage(named: "stake2"))
     let gateView = UIImageView(image: UIImage(named: "gate"))
@@ -44,14 +34,21 @@ class FirstViewController: UIViewController {
     self.view.addSubview(stack1View)
     self.view.addSubview(stack2View)
     self.view.addSubview(gateView)
+    
+    ballButton.addTarget(self, action: #selector(ballGravityFall), for: .touchUpInside)
+    ballButton.addTarget(self, action: #selector(ballDragOutside), for: .touchDragOutside)
+  }
+  
+  func ballDragOutside() {
+    times = times + 1
+    print(times)
   }
   
   func ballGravityFall() {
     dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
-    let gravityBehavior = UIGravityBehavior(items: [self.ballOutlet])
+    let gravityBehavior = UIGravityBehavior(items: [self.ballButton])
     gravityBehavior.magnitude = 2
     dynamicAnimator.addBehavior(gravityBehavior)
   }
 
 }
-
