c9b86ec3242e122d9b127f3978be7321eb64ead2
Yang Li <zjzsliyang@gmail.com>
2017-03-27 22:51:56 +0800

    fix(playgroundBook): fix bugs in plist and hide code

--- Yang Li's WWDC17 Apply.playgroundbook/Contents/Chapters/Mechanics.playgroundchapter/Pages/Brachistochrone.playgroundpage/Contents.swift
+++ Yang Li's WWDC17 Apply.playgroundbook/Contents/Chapters/Mechanics.playgroundchapter/Pages/Brachistochrone.playgroundpage/Contents.swift
@@ -1,14 +1,15 @@
+//#-hidden-code
 /*
  Copyright (C) 2016 Apple Inc. All Rights Reserved.
  See LICENSE.txt for this sample’s licensing information.
  */
 
- // Contents.swift
- // Created by Yang Li on 27/03/2017.
- //
- // Copyright © 2017 Yang Li, Tongji University, Shanghai.
- // All rights reserved.
- //
+// Contents.swift
+// Created by Yang Li on 27/03/2017.
+//
+// Copyright © 2017 Yang Li, Tongji University, Shanghai.
+// All rights reserved.
+//
 
 import UIKit
 import PlaygroundSupport
@@ -166,4 +167,4 @@ public func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor i
   }
 }
 
-
+//#-end-hidden-code
--- Yang Li's WWDC17 Apply.playgroundbook/Contents/Chapters/Mechanics.playgroundchapter/Pages/CircularMotion.playgroundpage/Contents.swift
+++ Yang Li's WWDC17 Apply.playgroundbook/Contents/Chapters/Mechanics.playgroundchapter/Pages/CircularMotion.playgroundpage/Contents.swift
@@ -1,14 +1,15 @@
+//#-hidden-code
 /*
  Copyright (C) 2016 Apple Inc. All Rights Reserved.
  See LICENSE.txt for this sample’s licensing information.
  */
 
- // Contents.swift
- // Created by Yang Li on 26/03/2017.
- //
- // Copyright © 2017 Yang Li, Tongji University, Shanghai.
- // All rights reserved.
- //
+// Contents.swift
+// Created by Yang Li on 26/03/2017.
+//
+// Copyright © 2017 Yang Li, Tongji University, Shanghai.
+// All rights reserved.
+//
 
 import UIKit
 import PlaygroundSupport
@@ -17,3 +18,5 @@ let CMViewController = UIViewController()
 let animationView = CMAnimationView(frame: CMViewController.view.bounds)
 CMViewController.view = animationView
 PlaygroundPage.current.liveView = CMViewController
+
+//#-end-hidden-code
